e6939af7e31047f9e698563c47d58dcb7f2378cb
liangyy <yanyu_18@sina.com>
2016-03-01 18:58:55 -0500

    Add path for plugin_script,py

--- source/conf.py
+++ source/conf.py
@@ -28,11 +28,15 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
+sys.path.append(os.path.abspath(''))
+
 extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
+    'plugin_script'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
