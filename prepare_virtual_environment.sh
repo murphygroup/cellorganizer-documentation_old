95b169862737c8f3694a82ff147d2cc5501c1225
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2016-03-11 18:03:25 -0500

    Updated installation script

--- prepare_virtual_environment.sh
+++ prepare_virtual_environment.sh
@@ -5,6 +5,9 @@ source ./bin/activate
 
 pip install numpy scipy matplotlib sphinx
 pip install -U ipython
+pip install --upgrade pip
+pip install xlrd
+pip install pandas
 
 git submodule init
 git submodule update
