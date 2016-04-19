f966cca481156bbe04dc1316563d9ee10a206a43
Yanyu Liang <yanyul@andrew.cmu.edu>
2016-04-19 15:23:55 -0400

    debug make_tabulate_for_excel

--- make_tabulate_from_excel.py
+++ make_tabulate_from_excel.py
@@ -1,10 +1,10 @@
 from tabulate import tabulate
-import pandas
+import pandas 
 import sys
 from os import getcwd
 
 inpt = sys.argv[1]
-df = pandas.io.excel.read_excel(inpt)
+df = pandas.read_excel(inpt, 0)
 df = df.fillna('N/A')
 body = df.values.tolist()
 header = df.columns.values.tolist()
