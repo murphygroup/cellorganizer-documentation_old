3e7fc11f96a7b652023e0e3b341f97f5dbf92d4c
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2018-03-07 01:59:14 -0500

    Changed theme

--- source/conf.py
+++ source/conf.py
@@ -108,11 +108,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'classic'
-html_theme_options = {
-    "rightsidebar": "true",
-    "relbarbgcolor": "black"
-}
+html_theme = 'bizstyle'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -124,10 +120,10 @@ html_theme_options = {
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = None
+html_title = 'CellOrganizer v2.7.0 documentation'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = 'CellOrganizer v2.7'
+html_short_title = 'CellOrganizer v2.7.0'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
