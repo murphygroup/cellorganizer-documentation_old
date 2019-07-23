7ecdf362c3fbf58a3618ab86d1146b9ce834b928
Ash Sankar <asankar21@gmail.com>
2019-07-23 16:47:07 -0400

    fix for the import

--- source/conf.py
+++ source/conf.py
@@ -122,7 +122,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme ='sphinx_rtd_theme' #'lsst_dd_rtd_theme' 'rtcat_sphinx_theme' 'neo_rtd_theme'
+html_theme ='sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -133,18 +133,17 @@ html_theme_options = {'responsive':True,
                       'titles_only': False,}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [sphinx_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'CellOrganizer v2.8.1 documentation'
+html_title = 'CellOrganizer v2.8.2 documentation'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = 'CellOrganizer v2.8.1'
+html_short_title = 'CellOrganizer v2.8.2'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'images/logo_red1.png' 
+html_logo = 'images/transp_logo.png' 
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
